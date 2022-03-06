9287f957fdc01f32bb8831376f91559a5c6b6b26
akemalsaglam <alikemalsaglam90@gmail.com>
2022-03-06 23:20:20 +0300

    first commit.

---
+++ src/main/java/com/aks/easycheckn/EasyChecknApplication.java
@@ -0,0 +1,13 @@
+package com.aks.easycheckn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyChecknApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyChecknApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aks/easycheckn/EasyChecknApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aks.easycheckn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyChecknApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
