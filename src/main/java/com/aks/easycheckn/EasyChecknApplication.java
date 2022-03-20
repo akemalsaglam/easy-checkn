6ceae141f9afd2b517cef97d6d5cce0418ec14c5
akemalsaglam <alikemalsaglam90@gmail.com>
2022-03-20 22:07:21 +0300

    use commons for extended scalars.

--- src/main/java/com/aks/easycheckn/EasyChecknApplication.java
+++ src/main/java/com/aks/easycheckn/EasyChecknApplication.java
@@ -1,9 +1,12 @@
 package com.aks.easycheckn;
 
+import com.aks.commons.graphql.GraphQLScalars;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(GraphQLScalars.class)
 public class EasyChecknApplication {
 
 	public static void main(String[] args) {
